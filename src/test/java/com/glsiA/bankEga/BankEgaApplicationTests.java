970ba6988e6c90da22ae91bea566332affcf0ee4
Dokpo Kwassi Princio <kwassidkp@gmail.com>
2025-12-27 13:46:32 +0000

    First Commit

---
+++ src/test/java/com/glsiA/bankEga/BankEgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glsiA.bankEga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankEgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
